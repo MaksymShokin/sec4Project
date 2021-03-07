7a78e5303f05456480dcd11cb25a754e1bad9839
Maksym <maksym.sh@upplabs.com>
2021-03-07 16:36:15 +0200

    using-filters-page

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,9 +1,29 @@
 import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../../dummy';
+import EventList from '../../../components/events/EventList.jsx';
 
 const EventFilterPage = () => {
   const { query } = useRouter();
+  const data = query.slug;
 
-  return <div>This is EventFilterPage</div>;
+  if (!data) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: +data[0],
+    month: +data[1]
+  });
+
+  if (!filteredEvents.length) {
+    return <div>No events found</div>;
+  }
+
+  return (
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
+  );
 };
 
 export default EventFilterPage;
